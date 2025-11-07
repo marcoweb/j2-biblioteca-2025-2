bfe3810b6fc492655934d3c517b66119f79f4dd2
ETEC <164@etec.sp.gov.br>
2025-11-07 16:59:56 +0000

    Aula 07/11/2025

--- src/main/java/application/controller/GeneroController.java
+++ src/main/java/application/controller/GeneroController.java
@@ -1,5 +1,7 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,13 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.Genero;
+import application.model.Livro;
 import application.repository.GeneroRepository;
+import application.repository.LivroRepository;
 
 @Controller
 @RequestMapping("/generos")
 public class GeneroController {
     @Autowired
     private GeneroRepository generoRepo;
+    @Autowired
+    private LivroRepository livroRepo;
 
     @RequestMapping("/list")
     public String list(Model ui) {
